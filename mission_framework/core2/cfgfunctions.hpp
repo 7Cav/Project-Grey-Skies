7f713f4dffb34b3e11101a1409e165c04ee2a1aa
PolySparrow <jackjr.longo@gmail.com>
2024-07-13 21:31:00 -0400

    Created cfgFunctions, updated description, and updated formatfile to be more inclusive per HR

---
+++ mission_framework/core2/cfgfunctions.hpp
@@ -0,0 +1,33 @@
+class core2
+{
+	class AO
+	{
+		file = "core2\AO";
+		class AO_LOC_GET {};
+	};
+	class DEBUG
+	{
+		file = "core2\DEBUG";
+
+	};
+	class MANAGERS
+	{
+		file = "core2\MANAGERS";
+		class GET_COMPS {};
+	};
+	class OBJ
+	{
+		file = "core2\MANAGERS";
+		class GET_COMPS {};
+	};
+	class OPFOR
+	{
+		file = "core2\OPFOR";
+		
+	};
+	class STRUCTURES
+	{
+		file = "core2\STRUCTURES";
+		class fn_STRUCTURE_TRANSLATOR {};
+	};
+};
